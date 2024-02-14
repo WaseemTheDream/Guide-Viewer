24e43c9cf28ba08aeb2e7f5073326ef88fd5ba1d
Waseem Ahmad <ahmad.w90@gmail.com>
2024-02-14 14:42:21 -0800

    Implement basic list rendering for guides

--- app/src/main/java/com/example/android/guideviewer/ui/common/LazyLoad.kt
+++ app/src/main/java/com/example/android/guideviewer/ui/common/LazyLoad.kt
@@ -1,4 +1,34 @@
 package com.example.android.guideviewer.ui.common
 
-class LazyLoad {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun PageLoader(
+    modifier: Modifier = Modifier,
+    loadingMessage: String) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = loadingMessage,
+            color = MaterialTheme.colorScheme.primary,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        CircularProgressIndicator(Modifier.padding(top = 10.dp))
+    }
 }
--- app/src/main/java/com/example/android/guideviewer/ui/guidelist/GuideListScreen.kt
+++ app/src/main/java/com/example/android/guideviewer/ui/guidelist/GuideListScreen.kt
@@ -1,12 +1,32 @@
 package com.example.android.guideviewer.ui.guidelist
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.android.guideviewer.R
 import com.example.android.guideviewer.data.model.ApiResult
+import com.example.android.guideviewer.data.model.Guide
 import com.example.android.guideviewer.data.model.GuideList
+import com.example.android.guideviewer.ui.common.PageLoader
 
 
 @Composable
@@ -16,8 +36,49 @@ fun GuideListScreen(
     val guides by viewModel.guideListState.collectAsState()
 
     when (guides) {
-        is ApiResult.Loading -> Text(text = "Loading")
+        is ApiResult.Loading ->
+            PageLoader(
+                modifier = Modifier.fillMaxSize(),
+                loadingMessage = stringResource(id = R.string.loading_data))
         is ApiResult.Failure -> Text(text = (guides as ApiResult.Failure).message ?: "Unknown Error")
-        is ApiResult.Success -> Text(text = "Success ${(guides as ApiResult.Success<GuideList>).value.guides[0].name}")
+        is ApiResult.Success -> GuideList(guides as ApiResult.Success<GuideList>)
+    }
+}
+
+@Composable
+fun GuideList(
+    guides: ApiResult.Success<GuideList>
+) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(20.dp)) {
+
+        item { Spacer(modifier = Modifier.padding(2.dp)) }
+
+        items(guides.value.guides.size) { index ->
+            ItemGuide(guide = guides.value.guides[index])
+        }
+
+        item { Spacer(modifier = Modifier.padding(2.dp)) }
+    }
+}
+
+@Composable
+fun ItemGuide(
+    guide: Guide
+) {
+    ElevatedCard(
+        modifier = Modifier.fillMaxWidth(0.9f),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)) {
+        Text(
+            modifier = Modifier.padding(top = 8.dp, start = 8.dp, end = 8.dp),
+            text = guide.name,
+            fontWeight = FontWeight.Bold,
+            fontSize = 12.sp)
+        Text(
+            modifier = Modifier.padding(bottom = 8.dp, start = 8.dp, end = 8.dp),
+            text = guide.startDate,
+            fontSize = 12.sp)
     }
 }
