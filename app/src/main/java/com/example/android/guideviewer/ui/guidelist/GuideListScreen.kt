7c2ef5bb48e1fbcf4f8ac44811d9cb47408aff01
Waseem Ahmad <ahmad.w90@gmail.com>
2024-02-14 14:57:42 -0800

    Add placeholder text for build variant

--- app/src/main/java/com/example/android/guideviewer/ui/guidelist/GuideListScreen.kt
+++ app/src/main/java/com/example/android/guideviewer/ui/guidelist/GuideListScreen.kt
@@ -1,5 +1,6 @@
 package com.example.android.guideviewer.ui.guidelist
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,9 +9,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -19,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -30,10 +36,35 @@ import com.example.android.guideviewer.ui.common.ErrorMessage
 import com.example.android.guideviewer.ui.common.PageLoader
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GuideListScreen(
-    viewModel: GuideListViewModel = hiltViewModel()
-) {
+fun GuideListScreen(viewModel: GuideListViewModel = hiltViewModel()) {
+    Scaffold(
+        topBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .background(MaterialTheme.colorScheme.primary),
+                verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = "Build Variant",
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    modifier = Modifier
+                        .padding(vertical = 16.dp, horizontal = 16.dp)
+                        .weight(1.0f),
+                    textAlign = TextAlign.Center)
+            }
+        }
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            GuideListScreenContent(viewModel = viewModel)
+        }
+    }
+}
+
+@Composable
+fun GuideListScreenContent(viewModel: GuideListViewModel) {
     val guides by viewModel.guideListState.collectAsState()
 
     when (guides) {
@@ -46,7 +77,9 @@ fun GuideListScreen(
                 (guides as ApiResult.Failure).message ?: stringResource(id = R.string.unknown_error)
             ErrorMessage(
                 message = errorMessage,
-                modifier = Modifier.fillMaxSize().padding(20.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp),
                 onClickRetry = { viewModel.getGuides() })
         }
         is ApiResult.Success ->
